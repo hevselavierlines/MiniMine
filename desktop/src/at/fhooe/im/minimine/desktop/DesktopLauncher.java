b6a101a10450b8a9bef839302652679aef565c07
Christine <scarletlefairy@yahoo.de>
2016-12-05 15:50:32 +0100

    Added a new libGDX project to the repository with a desktop launcher. This will be our working project. Feel free to contribute or create a branch. The world and exception packages have been removed now and will be added again in the next commit because of the different folder structure. Classpath issue with the assets folders already fixed.

---
+++ desktop/src/at/fhooe/im/minimine/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package at.fhooe.im.minimine.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import at.fhooe.im.minimine.MiniMine;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MiniMine(), config);
+	}
+}
