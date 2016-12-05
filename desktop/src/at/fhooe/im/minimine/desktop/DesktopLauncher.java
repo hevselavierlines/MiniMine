8d3940a35d71cb1e04b2a85500cd7cc542e60322
Christine <scarletlefairy@yahoo.de>
2016-12-05 15:56:46 +0100

    Added CubeTest and ShaderTest. Which one will be started can be easily switched in the DesktopLauncher.

--- desktop/src/at/fhooe/im/minimine/desktop/DesktopLauncher.java
+++ desktop/src/at/fhooe/im/minimine/desktop/DesktopLauncher.java
@@ -2,11 +2,14 @@ package at.fhooe.im.minimine.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import at.fhooe.im.minimine.CubeTest;
 import at.fhooe.im.minimine.MiniMine;
+import at.fhooe.im.minimine.ShaderTest;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MiniMine(), config);
+		new LwjglApplication(new CubeTest(), config);
 	}
 }
