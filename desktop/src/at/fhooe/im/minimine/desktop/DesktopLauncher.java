7ab962573023ac6900c168acf229ce217e2df369
Christine <scarletlefairy@yahoo.de>
2016-12-14 17:27:12 +0100

    wrong package imports after merge deleted

--- desktop/src/at/fhooe/im/minimine/desktop/DesktopLauncher.java
+++ desktop/src/at/fhooe/im/minimine/desktop/DesktopLauncher.java
@@ -3,8 +3,6 @@ package at.fhooe.im.minimine.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
-import at.fhooe.im.minimine.ManuelTest;
-import at.fhooe.im.minimine.MeshBuilderTest;
 import at.fhooe.im.minimine.MiniMine;
 import at.fhooe.im.minimine.meshbuilder.MeshBuilderTest;
 import at.fhooe.im.minimine.tutorials.CubeTest;
