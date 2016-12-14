95f904dab9b01dd82dc76bef41a6b5a389caae54
Afaci <baumi494@gmail.com>
2016-12-14 21:26:52 +0100

    full chunk renderer

---
+++ core/src/at/fhooe/im/minimine/ChunkMeshGenerator.java
@@ -0,0 +1,376 @@
+package at.fhooe.im.minimine;
+
+import java.util.LinkedList;
+
+import com.badlogic.gdx.graphics.Mesh;
+import com.badlogic.gdx.graphics.VertexAttribute;
+import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+
+import at.fhooe.im.minimine.exception.OutOfChunkBoundsException;
+import at.fhooe.im.minimine.world.Chunk;
+import at.fhooe.im.minimine.world.block.AirBlock;
+
+public class ChunkMeshGenerator {
+	public Mesh generateChunk(Chunk _chunk) {
+		LinkedList<Float> vertices = new LinkedList<Float>();
+		for(int x = -16; x <= Chunk.MAX_CHUNK_COORD_XZ; x++) {
+			for(int y = 0; y <= Chunk.MAX_CHUNK_COORD_Y; y++) {
+				for(int z = -16; z <= Chunk.MAX_CHUNK_COORD_XZ; z++) {
+					try {
+						if(_chunk.getBlockTypeAtChunkCoord(x, y, z) != AirBlock.class) {
+							//check above
+							if(y < 0 || _chunk.getBlockTypeAtChunkCoord(x, y + 1, z) == AirBlock.class) {
+								//draw the top side
+								vertices.add(x - 0.5f);
+								vertices.add(y + 0.5f);
+								vertices.add(z - 0.5f);
+								vertices.add(0.0f);
+								vertices.add(1.0f);
+								vertices.add(0.0f);
+								vertices.add(0.4f);
+								vertices.add(0.2f);
+								
+								vertices.add(x - 0.5f);
+								vertices.add(y + 0.5f);
+								vertices.add(z + 0.5f);
+								vertices.add(0.0f);
+								vertices.add(1.0f);
+								vertices.add(0.0f);
+								vertices.add(0.4f);
+								vertices.add(0.4f);
+								
+								vertices.add(x + 0.5f);
+								vertices.add(y + 0.5f);
+								vertices.add(z + 0.5f);
+								vertices.add(0.0f);
+								vertices.add(1.0f);
+								vertices.add(0.0f);
+								vertices.add(0.2f);
+								vertices.add(0.4f);
+								
+								vertices.add(x - 0.5f);
+								vertices.add(y + 0.5f);
+								vertices.add(z - 0.5f);
+								vertices.add(0.0f);
+								vertices.add(1.0f);
+								vertices.add(0.0f);
+								vertices.add(0.4f);
+								vertices.add(0.2f);
+								
+								vertices.add(x + 0.5f);
+								vertices.add(y + 0.5f);
+								vertices.add(z + 0.5f);
+								vertices.add(0.0f);
+								vertices.add(1.0f);
+								vertices.add(0.0f);
+								vertices.add(0.2f);
+								vertices.add(0.4f);
+								
+								vertices.add(x + 0.5f);
+								vertices.add(y + 0.5f);
+								vertices.add(z - 0.5f);
+								vertices.add(0.0f);
+								vertices.add(1.0f);
+								vertices.add(0.0f);
+								vertices.add(0.2f);
+								vertices.add(0.2f);
+							}
+							
+							if(y <= 0 || _chunk.getBlockTypeAtChunkCoord(x, y - 1, z) == AirBlock.class) {
+								vertices.add(x - 0.5f);
+								vertices.add(y - 0.5f);
+								vertices.add(z - 0.5f);
+								vertices.add(0.0f);
+								vertices.add(-1.0f);
+								vertices.add(0.0f);
+								vertices.add(0.0f);
+								vertices.add(0.0f);
+								
+								vertices.add(x + 0.5f);
+								vertices.add(y - 0.5f);
+								vertices.add(z + 0.5f);
+								vertices.add(0.0f);
+								vertices.add(-1.0f);
+								vertices.add(0.0f);
+								vertices.add(0.2f);
+								vertices.add(0.2f);
+								
+								vertices.add(x - 0.5f);
+								vertices.add(y - 0.5f);
+								vertices.add(z + 0.5f);
+								vertices.add(0.0f);
+								vertices.add(-1.0f);
+								vertices.add(0.0f);
+								vertices.add(0.0f);
+								vertices.add(0.2f);
+								
+								vertices.add(x - 0.5f);
+								vertices.add(y - 0.5f);
+								vertices.add(z - 0.5f);
+								vertices.add(0.0f);
+								vertices.add(-1.0f);
+								vertices.add(0.0f);
+								vertices.add(0.0f);
+								vertices.add(0.0f);
+								
+								vertices.add(x + 0.5f);
+								vertices.add(y - 0.5f);
+								vertices.add(z - 0.5f);
+								vertices.add(0.0f);
+								vertices.add(-1.0f);
+								vertices.add(0.0f);
+								vertices.add(0.2f);
+								vertices.add(0.0f);
+								
+								vertices.add(x + 0.5f);
+								vertices.add(y - 0.5f);
+								vertices.add(z + 0.5f);
+								vertices.add(0.0f);
+								vertices.add(-1.0f);
+								vertices.add(0.0f);
+								vertices.add(0.2f);
+								vertices.add(0.2f);
+							}
+							if(z <= -16 || _chunk.getBlockTypeAtChunkCoord(x, y, z - 1) ==  AirBlock.class) {
+								vertices.add(x - 0.5f);
+								vertices.add(y - 0.5f);
+								vertices.add(z - 0.5f);
+								vertices.add(0.0f);
+								vertices.add(0.0f);
+								vertices.add(-1.0f);
+								vertices.add(0.4f);
+								vertices.add(0.4f);
+								
+								vertices.add(x - 0.5f);
+								vertices.add(y + 0.5f);
+								vertices.add(z - 0.5f);
+								vertices.add(0.0f);
+								vertices.add(0.0f);
+								vertices.add(-1.0f);
+								vertices.add(0.4f);
+								vertices.add(0.6f);
+								
+								vertices.add(x + 0.5f);
+								vertices.add(y + 0.5f);
+								vertices.add(z - 0.5f);
+								vertices.add(0.0f);
+								vertices.add(0.0f);
+								vertices.add(-1.0f);
+								vertices.add(0.6f);
+								vertices.add(0.6f);
+								
+								vertices.add(x - 0.5f);
+								vertices.add(y - 0.5f);
+								vertices.add(z - 0.5f);
+								vertices.add(0.0f);
+								vertices.add(0.0f);
+								vertices.add(-1.0f);
+								vertices.add(0.4f);
+								vertices.add(0.4f);
+								
+								vertices.add(x + 0.5f);
+								vertices.add(y + 0.5f);
+								vertices.add(z - 0.5f);
+								vertices.add(0.0f);
+								vertices.add(0.0f);
+								vertices.add(-1.0f);
+								vertices.add(0.6f);
+								vertices.add(0.6f);
+								
+								vertices.add(x + 0.5f);
+								vertices.add(y - 0.5f);
+								vertices.add(z - 0.5f);
+								vertices.add(0.0f);
+								vertices.add(0.0f);
+								vertices.add(-1.0f);
+								vertices.add(0.6f);
+								vertices.add(0.4f);
+							}
+							
+							if(z >= 16 || _chunk.getBlockTypeAtChunkCoord(x, y, z + 1) == AirBlock.class) {
+								vertices.add(x - 0.5f);
+								vertices.add(y - 0.5f);
+								vertices.add(z + 0.5f);
+								vertices.add(0.0f);
+								vertices.add(0.0f);
+								vertices.add(1.0f);
+								vertices.add(0.4f);
+								vertices.add(0.4f);
+								
+								vertices.add(x + 0.5f);
+								vertices.add(y - 0.5f);
+								vertices.add(z + 0.5f);
+								vertices.add(0.0f);
+								vertices.add(0.0f);
+								vertices.add(1.0f);
+								vertices.add(0.4f);
+								vertices.add(0.6f);
+								
+								vertices.add(x + 0.5f);
+								vertices.add(y + 0.5f);
+								vertices.add(z + 0.5f);
+								vertices.add(0.0f);
+								vertices.add(0.0f);
+								vertices.add(1.0f);
+								vertices.add(0.6f);
+								vertices.add(0.6f);
+								
+								vertices.add(x - 0.5f);
+								vertices.add(y - 0.5f);
+								vertices.add(z + 0.5f);
+								vertices.add(0.0f);
+								vertices.add(0.0f);
+								vertices.add(1.0f);
+								vertices.add(0.4f);
+								vertices.add(0.4f);
+								
+								vertices.add(x + 0.5f);
+								vertices.add(y + 0.5f);
+								vertices.add(z + 0.5f);
+								vertices.add(0.0f);
+								vertices.add(0.0f);
+								vertices.add(1.0f);
+								vertices.add(0.6f);
+								vertices.add(0.6f);
+								
+								vertices.add(x - 0.5f);
+								vertices.add(y + 0.5f);
+								vertices.add(z + 0.5f);
+								vertices.add(0.0f);
+								vertices.add(0.0f);
+								vertices.add(1.0f);
+								vertices.add(0.6f);
+								vertices.add(0.4f);
+							}
+							
+							if(x <= -16 || _chunk.getBlockTypeAtChunkCoord(x - 1, y, z) == AirBlock.class) {
+								vertices.add(x - 0.5f);
+								vertices.add(y - 0.5f);
+								vertices.add(z - 0.5f);
+								vertices.add(-1.0f);
+								vertices.add(0.0f);
+								vertices.add(0.0f);
+								vertices.add(0.6f);
+								vertices.add(0.6f);
+								
+								vertices.add(x - 0.5f);
+								vertices.add(y - 0.5f);
+								vertices.add(z + 0.5f);
+								vertices.add(-1.0f);
+								vertices.add(0.0f);
+								vertices.add(0.0f);
+								vertices.add(0.6f);
+								vertices.add(0.8f);
+								
+								vertices.add(x - 0.5f);
+								vertices.add(y + 0.5f);
+								vertices.add(z + 0.5f);
+								vertices.add(-1.0f);
+								vertices.add(0.0f);
+								vertices.add(0.0f);
+								vertices.add(0.8f);
+								vertices.add(0.8f);
+								
+								vertices.add(x - 0.5f);
+								vertices.add(y - 0.5f);
+								vertices.add(z - 0.5f);
+								vertices.add(-1.0f);
+								vertices.add(0.0f);
+								vertices.add(0.0f);
+								vertices.add(0.6f);
+								vertices.add(0.6f);
+								
+								vertices.add(x - 0.5f);
+								vertices.add(y + 0.5f);
+								vertices.add(z + 0.5f);
+								vertices.add(-1.0f);
+								vertices.add(0.0f);
+								vertices.add(0.0f);
+								vertices.add(0.8f);
+								vertices.add(0.8f);
+								
+								vertices.add(x - 0.5f);
+								vertices.add(y + 0.5f);
+								vertices.add(z - 0.5f);
+								vertices.add(-1.0f);
+								vertices.add(0.0f);
+								vertices.add(0.0f);
+								vertices.add(0.8f);
+								vertices.add(0.6f);
+							}
+							
+							if(x >= 16 || _chunk.getBlockTypeAtChunkCoord(x + 1, y, z) == AirBlock.class) {
+								vertices.add(x + 0.5f);
+								vertices.add(y - 0.5f);
+								vertices.add(z - 0.5f);
+								vertices.add(+1.0f);
+								vertices.add(0.0f);
+								vertices.add(0.0f);
+								vertices.add(0.8f);
+								vertices.add(0.8f);
+								
+								vertices.add(x + 0.5f);
+								vertices.add(y + 0.5f);
+								vertices.add(z - 0.5f);
+								vertices.add(1.0f);
+								vertices.add(0.0f);
+								vertices.add(0.0f);
+								vertices.add(1.0f);
+								vertices.add(0.8f);
+								
+								vertices.add(x + 0.5f);
+								vertices.add(y + 0.5f);
+								vertices.add(z + 0.5f);
+								vertices.add(1.0f);
+								vertices.add(0.0f);
+								vertices.add(0.0f);
+								vertices.add(1.0f);
+								vertices.add(1.0f);
+								
+								vertices.add(x + 0.5f);
+								vertices.add(y - 0.5f);
+								vertices.add(z - 0.5f);
+								vertices.add(1.0f);
+								vertices.add(0.0f);
+								vertices.add(0.0f);
+								vertices.add(0.8f);
+								vertices.add(0.8f);
+								
+								vertices.add(x + 0.5f);
+								vertices.add(y + 0.5f);
+								vertices.add(z + 0.5f);
+								vertices.add(1.0f);
+								vertices.add(0.0f);
+								vertices.add(0.0f);
+								vertices.add(1.0f);
+								vertices.add(1.0f);
+								
+								vertices.add(x + 0.5f);
+								vertices.add(y - 0.5f);
+								vertices.add(z + 0.5f);
+								vertices.add(1.0f);
+								vertices.add(0.0f);
+								vertices.add(0.0f);
+								vertices.add(0.8f);
+								vertices.add(1.0f);
+							}
+						}
+						
+					} catch (OutOfChunkBoundsException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		float[] meshInfo = new float[vertices.size()];
+		int i = 0;
+		for(Float meshData : vertices) {
+			meshInfo[i++] = meshData;
+		}
+		System.out.println(i);
+		Mesh mesh = new Mesh(true, i, 0, new VertexAttribute(Usage.Position, 3, "a_position"), new VertexAttribute(Usage.Normal,
+				3, "a_normal"), new VertexAttribute(Usage.TextureCoordinates, 2, "a_texcoords"));
+		mesh.setVertices(meshInfo);
+		return mesh;
+	}
+}
