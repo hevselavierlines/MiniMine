b140708967f3ddb2305d730b9590f19679eb370c
Michael Staudinger <M.Staudinger.92@gmail.com>
2017-01-09 14:01:08 +0100

    Added TNT Block + UphillBiomeGenerator works now with 1 existing neighbour

--- core/src/at/fhooe/im/minimine/world/biome/generator/UphillBiomeGenerator.java
+++ core/src/at/fhooe/im/minimine/world/biome/generator/UphillBiomeGenerator.java
@@ -50,13 +50,113 @@ public class UphillBiomeGenerator extends AbstractBiomeGenerator {
 	}
 	
 	private Chunk generateWithOneNeighbour(Chunk newChunk, Chunk neighbour, int neighbourPos) {
-		int startHeight = 0;
 		switch (neighbourPos) {
 			case AbstractBiomeGenerator.NORTH_NEIGHBOUR: {
-				
+				for (int x = -16; x <= 16; x++) {
+					for (int z = 16; z >= -16; z--) {
+						// get old height
+						int height;
+						if (z == 16) {
+							height = neighbour.getMaximumHeightAtChunkCoord(x, -16);
+							
+						} else {
+							height = neighbour.getMaximumHeightAtChunkCoord(x, z + 1);
+						}
+						// generate new height
+						Random rand = new Random();
+						int caseH = rand.nextInt(5);
+						switch(caseH) {
+							case 0: break;
+							case 1: case 2: case 3: height++; break;
+							case 4: height += 2; break;
+							default: break;
+						}
+						// apply new height
+						newChunk.fillChunkArrayCoordUp(DirtBlock.class, x, height, z);
+					}
+				}
+				break;
+			}
+			case AbstractBiomeGenerator.EAST_NEIGHBOUR: {
+				for (int z = 16; z >= 16; z--) {
+					for (int x = -16; x <= 16; x++) {
+						// get old height
+						int height;
+						if (x == -16) {
+							height = neighbour.getMaximumHeightAtChunkCoord(16, z);
+							
+						} else {
+							height = neighbour.getMaximumHeightAtChunkCoord(x - 1, z);
+						}
+						// generate new height
+						Random rand = new Random();
+						int caseH = rand.nextInt(5);
+						switch(caseH) {
+							case 0: break;
+							case 1: case 2: case 3: height++; break;
+							case 4: height += 2; break;
+							default: break;
+						}
+						// apply new height
+						newChunk.fillChunkArrayCoordUp(DirtBlock.class, x, height, z);
+					}
+				}
+				break;
+			}
+			case AbstractBiomeGenerator.SOUTH_NEIGHBOUR: {
+				for (int x = -16; x <= 16; x++) {
+					for (int z = -16; z <= 16; z++) {
+						// get old height
+						int height;
+						if (z == -16) {
+							height = neighbour.getMaximumHeightAtChunkCoord(x, 16);
+							
+						} else {
+							height = neighbour.getMaximumHeightAtChunkCoord(x, z - 1);
+						}
+						// generate new height
+						Random rand = new Random();
+						int caseH = rand.nextInt(5);
+						switch(caseH) {
+							case 0: break;
+							case 1: case 2: case 3: height++; break;
+							case 4: height += 2; break;
+							default: break;
+						}
+						// apply new height
+						newChunk.fillChunkArrayCoordUp(DirtBlock.class, x, height, z);
+					}
+				}
+				break;
+			}
+			case AbstractBiomeGenerator.WEST_NEIGHBOUR: {
+				for (int z = 16; z >= 16; z--) {
+					for (int x = 16; x >= -16; x--) {
+						// get old height
+						int height;
+						if (x == 16) {
+							height = neighbour.getMaximumHeightAtChunkCoord(-16, z);
+							
+						} else {
+							height = neighbour.getMaximumHeightAtChunkCoord(x + 1, z);
+						}
+						// generate new height
+						Random rand = new Random();
+						int caseH = rand.nextInt(5);
+						switch(caseH) {
+							case 0: break;
+							case 1: case 2: case 3: height++; break;
+							case 4: height += 2; break;
+							default: break;
+						}
+						// apply new height
+						newChunk.fillChunkArrayCoordUp(DirtBlock.class, x, height, z);
+					}
+				}
 				break;
 			}
 		}
+		return newChunk;
 	}
 	
 }
