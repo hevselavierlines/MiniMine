770bae47d2527afebbf90c701c30e050f79ec529
Afaci <baumi494@gmail.com>
2017-01-11 12:05:17 +0100

    merged ui in character and added main class

--- desktop/src/at/fhooe/im/minimine/desktop/DesktopLauncher.java
+++ desktop/src/at/fhooe/im/minimine/desktop/DesktopLauncher.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 import at.fhooe.im.minimine.CharacterTest;
+import at.fhooe.im.minimine.Main;
 import at.fhooe.im.minimine.ManuelTest;
 import at.fhooe.im.minimine.MiniMine;
 import at.fhooe.im.minimine.StaudiTest;
@@ -17,6 +18,6 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new 	LwjglApplicationConfiguration();
 
-		new LwjglApplication(new UITest(), config);
+		new LwjglApplication(new Main(), config);
 	}
 }
